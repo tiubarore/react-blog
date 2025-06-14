804f3279b1c74d6d766faaf77c40af462c73924a
tiubarore <tiubarore93@gmail.com>
2025-06-14 15:24:58 +0200

    edit navbar

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,14 +3,14 @@ import { Link } from "react-router";
 function Nav({ search, setSearch }) {
   return (
     <nav className="text-center bg-blue-800 text-white p-2 flex">
-      <ul className="flex gap-2 justify-center p-2 mx-auto">
-        <li className="hover:bg-blue-400 p-1.5 rounded mx-2">
+      <ul className="flex sm:gap-3 gap-5  justify-center p-2 mx-auto">
+        <li className="hover:bg-blue-400 lg:p-1.5  rounded sm:mx-1.5 lg:mx-2">
           <Link to="/">Home</Link>
         </li>
-        <li className="hover:bg-blue-400 p-1.5 rounded mx-2">
+        <li className="hover:bg-blue-400 lg:p-1.5 rounded  sm:mx-1.5 lg:mx-">
           <Link to="/post">New Post</Link>
         </li>
-        <li className="hover:bg-blue-400 p-1.5 rounded mx-2">
+        <li className="hover:bg-blue-400 lg:p-1.5 rounded  sm:mx-1.5 lg:mx-2">
           <Link to="/about">About</Link>
         </li>
       </ul>
@@ -19,7 +19,9 @@ function Nav({ search, setSearch }) {
         className="searchForm self-center mx-auto"
         onSubmit={(e) => e.preventDefault()}
       >
-        <label htmlFor="search">Search</label>
+        <label htmlFor="search" className="">
+          Search
+        </label>
         <input
           className="border ml-3 p-1 rounded"
           type="text"
