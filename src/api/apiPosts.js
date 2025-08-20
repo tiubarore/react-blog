67e1a1d767da9fb9aad0537a0f05255d957ea4e4
Vincenzo Nicassio <tiubarore93@gmail.com>
2025-08-20 15:52:00 +0200

    error path

--- src/api/apiPosts.js
+++ src/api/apiPosts.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://my-json-server.typicode.com/tiubarore/blog-react",
+  baseURL: "https://my-json-server.typicode.com/tiubarore/react-blog",
 });
